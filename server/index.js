21272f6d761fe615c6839586585f173f3c3c5136
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-29 13:47:31 -0500

    init express server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const config = require('../config');
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
+});
+
+app.listen(config.port, () => {
+  console.log(`listening on port ${config.port}`);
+});
