94cdcd8eedbf4f1bca6491b201acdebc52715a5f
Kacy Holm <[email>
2022-01-29 15:09:43 -0700

    Merged with main, implemented login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Messages from './components/Messages.jsx';
 import PostRide from './components/PostRide.jsx';
 import Profile from './components/Profile.jsx';
 import SignUp from './components/SignUp.jsx';
-//import Login from './components/Login.jsx'
+import Login from './components/Login.jsx'
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
       <Menu />
       <Routes>
         <Route path="/" exact element={<Main />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/my-profile" element={<Profile />} />
         <Route path="/messages" element={<Messages />} />
