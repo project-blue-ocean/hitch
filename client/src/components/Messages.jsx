5dd6e1b75b81b242758abd7a4167df1e610f5df3
Regina Grogan <regina.r.grogan@gmail.com>
2022-01-30 16:11:15 -0600

    Changed Messages component style

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -1,5 +1,20 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Grid,
+  Paper,
+  Divider,
+  TextField,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Avatar,
+  Fab,
+} from '@material-ui/core';
+import Button from '@mui/material/Button';
+import Message from './Message.jsx';
 import dummyData from '../../../messagesDummyData';
 
 function Messages({userId}) {
@@ -16,18 +31,52 @@ function Messages({userId}) {
 
   return (
     <div>
-      <div>
-        {usersContacted.map((user) => <div>{user.senderName}</div>)}
-      </div>
-      <div>
-        {messages.map((message) => <div>{message.msg}</div>)}
-      </div>
+      <Grid container component={Paper}>
+        <Grid item xs={3}>
+          <List>
+            {usersContacted.map((user) => (
+              <ListItem button>
+                <ListItemIcon>
+                  <Avatar alt={user.senderName} src="" />
+                </ListItemIcon>
+                <ListItemText primary={user.senderName}>{user.senderName}</ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+        <Divider />
+        <Grid item xs={9}>
+          <List>
+            {messages.map((message) => (
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={12}>
+                    <ListItemText align="right" primary={message.msg} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <ListItemText align="right" secondary={message.time} />
+                  </Grid>
+                </Grid>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <Grid container style={{padding: '20px'}}>
+            <Grid item xs={11}>
+              <TextField id="outlined-basic-email" label="Type Something" fullWidth />
+            </Grid>
+            <Grid xs={1} align="right">
+              <Fab color="primary" aria-label="add">Send</Fab>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
-Messages.propTypes = {
-  userId: PropTypes.number.isRequired,
-};
+// Messages.propTypes = {
+//   userId: PropTypes.number.isRequired,
+// };
 
 export default Messages;
