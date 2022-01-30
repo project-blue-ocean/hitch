75cbcd7f1d34754543f95d73d2174f14105a347b
Kacy Holm <[email>
2022-01-30 10:18:15 -0700

    Photo and name rendering in profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function Profile() {
-  return <div>This is the MyProfile view.</div>;
+  return (
+    <Container component="main" maxWidth="auto">
+      <CssBaseline />
+      <Grid container
+        spacing={2}
+        direction="column"
+        alignItems="center">
+        <Box sx={{
+          width: 250,
+          height: 250,
+          borderRadius: 1,
+          backgroundImage: 'url(https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg)',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+        />
+        <Grid item>
+          <Typography component="h1" variant="h5">
+            Name
+          </Typography>
+        </Grid>
+        <Grid item xs={6} md={4}>
+          <Item>xs=6 md=4</Item>
+        </Grid>
+        <Grid item xs={6} md={8}>
+          <Item>xs=6 md=8</Item>
+        </Grid>
+      </Grid>
+
+    </Container>
+  );
 }
 
 export default Profile;
