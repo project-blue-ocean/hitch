2250be170ace36c2cf2fb07b13bf6d16f6140e41
Regina Grogan <regina.r.grogan@gmail.com>
2022-01-30 13:41:23 -0600

    Changed Messages component to render userscontacted and messages from dummy data

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import dummyData from '../../../messagesDummyData';
 
-function Messages() {
-  return <div>This is the Messages view.</div>;
+function Messages({userId}) {
+  const [usersContacted, setUsersContacted] = useState(dummyData.usersContacted);
+  const [messages, setMessages] = useState(dummyData.messages);
+
+  // const getUsersContacted = () => {
+  //   return false;
+  // };
+
+  // const getMessages = (userContactedId) => {
+  //   return false;
+  // };
+
+  return (
+    <div>
+      <div>
+        {usersContacted.map((user) => <div>{user.senderName}</div>)}
+      </div>
+      <div>
+        {messages.map((message) => <div>{message.msg}</div>)}
+      </div>
+    </div>
+  );
 }
+
+Messages.propTypes = {
+  userId: PropTypes.number.isRequired,
+};
 
 export default Messages;
