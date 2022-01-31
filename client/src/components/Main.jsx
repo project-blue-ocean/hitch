bb233f95e3c00487103d214c68a4e1aeb64d856a
Cory Zauss <coryclassic1989@gmail.com>
2022-01-30 21:29:10 -0500

    updates search term

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import sampleData from '../../../server/rideSamples.js';
 
 function Main() {
-  return <div>This is the Home view.</div>;
+  const [searchTerm, setSearchTerm] = useState({});
+
+  const updateSearch = (e) => {
+    const { value } = e.target;
+    const name = e.target.getAttribute('name');
+    if (name === 'From') {
+      setSearchTerm({ ...searchTerm, source: value.toLowerCase() });
+    } else if (name === 'Destination') {
+      setSearchTerm({ ...searchTerm, destination: value.toLowerCase() });
+    }
+  };
+
+  const searchRides = async (e) => {
+    e.preventDefault();
+    console.log(searchTerm);
+    axios.get('/rides', { params: { search: searchTerm } });
+  };
+
+  return (
+    <div>
+      <form onSubmit={searchRides}>
+        <input
+          type="text"
+          name="From"
+          placeholder="From"
+          onChange={updateSearch}
+        />
+        <input
+          type="text"
+          name="Destination"
+          placeholder="Destination"
+          onChange={updateSearch}
+        />
+        <button type="submit" onClick={searchRides}>
+          {' '}
+          Go!
+          {' '}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Main;
