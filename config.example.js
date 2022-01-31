9132498310819eead7a28fc402bffe1ed5f07369
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-31 16:22:58 -0500

    add example config.js file

---
+++ config.example.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: 3000,
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+  },
+};
