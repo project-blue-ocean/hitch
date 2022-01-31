c1aae039fb7f4057fd736d34829920b5f3b5e1c4
Regina Grogan <regina.r.grogan@gmail.com>
2022-01-31 16:01:16 -0600

    Changed car year range

--- client/src/components/PostRide.jsx
+++ client/src/components/PostRide.jsx
@@ -62,7 +62,7 @@ function PostRide({ userId }) {
             </Grid>
             <Grid item xs={12} sm={6}>
               <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker views={['year']} id="year" name="year" label="Year only" value={date} minDate={new Date('2012-03-01')} maxDate={new Date('2023-06-01')} onChange={(newValue) => setDate(newValue)} renderInput={(params) => <TextField {...params} helperText={null} />} />
+                <DatePicker views={['year']} id="year" name="year" label="Year only" value={date} minDate={new Date('1950-01-01')} maxDate={new Date('2023-02-01')} onChange={(newValue) => setDate(newValue)} renderInput={(params) => <TextField {...params} helperText={null} />} />
               </LocalizationProvider>
             </Grid>
             <Grid item xs={12} sm={6}>
