348de0c23401c5aef658d21534cfedbf95f6d239
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-02-01 18:38:32 -0500

    import firebase/storage

--- client/src/firebase/index.js
+++ client/src/firebase/index.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 import config from '../../../config';
 
 // Initialize Firebase
@@ -16,3 +17,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
